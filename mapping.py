f840f7267f6da86c5915ad42fe3831b0aa92ecab
Romain Feron <romain.feron@evobio.eu>
2017-09-06 15:19:26 +0200

    Fixed small path bugs

--- mapping.py
+++ mapping.py
@@ -7,20 +7,20 @@ import utils
 def generate_shell_files():
 
     file_pairs = utils.find_pairs(READS_PATH)
-    qsub_file = open(REMAPPING_SH, 'w')
+    qsub_file = open(MAPPING_SH, 'w')
     for pair in file_pairs:
         info = utils.reads_file_info(pair[0])
-        shell_file_path = mapping_sh(infos)
+        shell_file_path = mapping_sh(info)
         shell_file = open(shell_file_path, 'w')
         genotoul.header(shell_file,
-                        name='mapping_' + sex + '_' + lane,
+                        name='mapping_' + info['sex'] + '_' + info['lane'],
                         threads=settings.threads)
         shell_file.write('bwa mem' +
                          ' -t ' + str(settings.threads) +
-                         ' ' + genome +
+                         ' ' + GENOME_PATH +
                          ' ' + pair[0] +
                          ' ' + pair[1] +
                          ' | samtools view -b -' +
-                         ' >' + remapping_output(info) + '\n')
+                         ' >' + mapping_output(info) + '\n')
         shell_file.close()
         qsub_file.write('qsub ' + shell_file_path + '\n')
