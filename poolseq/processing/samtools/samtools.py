61c90321633ae1492d8f4ae27d808c5b5814fe19
Romain Feron <romain.feron@evobio.eu>
2017-09-21 17:05:22 +0200

    Added samtools index module

--- poolseq/processing/samtools/samtools.py
+++ poolseq/processing/samtools/samtools.py
@@ -1,7 +1,9 @@
+from poolseq.processing.samtools.index import Index
 from poolseq.processing.samtools.mpileup import Mpileup
 
 
 class Samtools():
 
     def __init__(self, data):
+        self.index = Index(data)
         self.mpileup = Mpileup(data)
